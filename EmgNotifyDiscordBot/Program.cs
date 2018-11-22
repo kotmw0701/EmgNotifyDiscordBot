37074f5e1b2bafed3ab183dde04c9ca8170b6bde
kotmw0701 <supurasyu@hotmail.co.jp>
2018-11-22 22:52:35 +0900

    プロジェクト ファイルを追加します。

---
+++ EmgNotifyDiscordBot/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace EmgNotifyDiscordBot {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
